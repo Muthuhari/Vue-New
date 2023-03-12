7db282aa3b8c2f1a3b21f9d86239ba7b7c7f5f12
MUthuhari <muthuharinisansala2@gmail.com>
2023-03-13 00:35:14 +0530

    Update styles

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
-<!-- <script setup>
+<!--
+  Composition API (new)
+  <script setup>
 import {ref} from "vue"
 
 let count = ref(0)
@@ -34,14 +36,14 @@ main {
 <template>
 <main>
       <container class="container">
-        <h4>the current count is...</h4>
+        <h2>The current count is..</h2>
         <h1>{{count}}</h1>
-        <button @click="count++">+</button>
-        <button @click="count--">-</button>
+        <button @click="addToCount()">+</button>
+        <button @click="subtractFromCount()">-</button>
       </container>
   </main>
 </template>
-
+{/*Option API (Odl, but use in company- here use export default.........) */}
 <script >
 export default({
   data(){
@@ -51,7 +53,10 @@ export default({
   },
   methods: {
     addToCount(){
-      console.log("hellooooo")
+      this.count = this.count +1
+    },
+    subtractFromCount(){
+      this.count = this.count -1
     }
   }
 })
@@ -59,6 +64,7 @@ export default({
 
 <style scoped>
 main {
+  
   width: 100vw;
   height: 100vh;
   background-color: aliceblue;
@@ -70,5 +76,16 @@ main {
 .container {
   text-align: center;
 }
-
+button {
+  font-size: 6em;
+  background-color: blue;
+  color: white;
+}
+h2{
+  font-size: 3em;
+} 
+h1{
+  font-size: 5em;
+  color: green;
+} 
 </style>
